737f46030d8c7b480095019853fa7c94974b2549
MeFahme <yusrianfahmi15@gmail.com>
2024-10-05 22:10:34 +0800

    Add files via upload

---
+++ Kelas/Pertemuan ke 3/Pertemuan_ke_5.py
@@ -0,0 +1,80 @@
+# list = ["kuda", 2 ,True, ]
+# print (list)
+
+# print [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+# for i in range
+
+# nama = ["Celio", "Afriza", "Farrel", "Ghazali"]
+# print (len(nama))
+
+# for i in range(len(nama)):
+#   print(nama[i])
+
+#list mata kuliah yang sulit di semester 1
+# matkul = ["Kalkulus", "Fisika Dasar", "Pengantar Teknologi Informasi", "kalkulus"]
+# print(matkul)
+# print()
+
+# matkul.append("Sastra Mesin")
+# print(matkul)
+
+# matkul.insert(1, "sastra jawa")
+# print(matkul)
+
+# matkul[1] = "fisika jawa"
+# print(matkul)
+
+# del matkul [1]
+# print(matkul)
+
+# buang = matkul.pop(1)
+# print(matkul)
+
+# print(f"matkul yg dibuang = {buang}")
+
+# print(matkul[0:2])
+
+# prodi = ["Informatika", "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", "Teknik Pertambangan", "Teknik Elektro", "Teknik Industri", "Teknik Sipil", "Teknik Geologi", "Teknik Kimia"]
+# #kita ingin menampilkan semua elemen di dalam list dengan jarak 1 elemen diantaranya
+# print(prodi[::2], "\n")
+# #kita akan mengambil prodi Sistem Informasi sampai Teknik Geologi dengan 1 langkah setiap elemen di antaranya
+# print(prodi[1:8:2])
+
+# prodi = [1,2,3,4,5,6,7,8,9,10]
+# #kita ingin menampilkan semua elemen di dalam list dengan jarak 1 elemen diantaranya
+# print(prodi[::2], "\n")
+# #kita akan mengambil prodi Sistem Informasi sampai Teknik Geologi dengan 1 langkah setiap elemen di antaranya
+# print(prodi[1:10:2])
+
+#list mobil bahan bakar listrik
+# bensin = ["avanza", "honda", "yamaha"]
+# #list mobil listrik
+# listrik = ["tesla", "SAIC"]
+# #menggabungkan listrik dan bensin
+# semua = bensin+listrik
+# #menampilkan list semua
+# print(semua)
+
+#list barang
+# barang = [ ["sepatu", "tas", "baju"], 
+#           ["Pulpen", "Pensil", "Laptop"] ]
+# #memanggil list secara keseluruhan untuk melihat bagaimana outputnya
+# # print(barang, "\n")
+# # #memanggil indeks pertama
+# # print(barang[0], "\n")
+# #kali ini kita ingin menampilkan elemen "Pulpen", kita perlu mengakses indeks dua kali, yang pertama untuk memilih list dengan elemen yang kita inginkan dan yang kedua indekselemen itu sendiri
+# # print(barang[-2][-1])
+
+# for baris in barang:
+#   for kolom in baris:
+#     print(kolom)
+#   # print (i)
+
+# nama = ("udin", "android", "jajang")
+
+# # nama2 = ("eko", "ipon", "sukma")
+# # nama3 = nama1+nama2
+# # print(nama3)
+# print (nama[1])
+# print (nama[2])
+
